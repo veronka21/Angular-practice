f55f6ac48218fe56ac006f8c3839767c32d2382a
Veronika Takács <roni9764@gmail.com>
2021-02-10 14:54:07 +0100

    Create custom directive for toggling dropdown menus

---
+++ course-project/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from "@angular/core";
+
+@Directive({
+    selector: '[appDropdown]'    
+})
+export class DropdownDirective {
+    @HostBinding('class.open') isOpen: boolean = false;
+
+    constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+    @HostListener('click') toggleOpen(eventData: Event) {
+        this.isOpen = !this.isOpen;
+    }
+
+}
