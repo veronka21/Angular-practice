80ac21aba01a7eea320527591b3793a230d2bead
Veronika Takács <roni9764@gmail.com>
2021-02-15 00:23:46 +0100

    Complete observables exercise

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubsription: Subscription;
 
   constructor() { }
-
+  
   ngOnInit() {
-  }
+    // const myNumbers = interval(1000);
+    // this.firstObsSubsription = myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   })
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count == 2) observer.complete();
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
+        count++;
+      } ,1000)
+    })
 
+    customIntervalObservable.pipe(map((data: number) => {
+      return 'Round: ' + (data + 1)
+    }))
+
+    this.firstObsSubsription = customIntervalObservable.pipe(filter(data => {
+      return data > 0;
+    }), map((data: number) => {
+      return 'Round: ' + (data + 1)
+    })).subscribe(data => {
+      console.log(data);
+    }, (error) => {
+      console.log(error);
+      alert(error.message);
+    }, () => console.log("completed"))
+  }
+      
+  ngOnDestroy(): void {
+    this.firstObsSubsription.unsubscribe();
+  }
 }
+    
